7124c339756d3410dbf20d32edd221db1ffc5ea3
Pedrão Felas <102636895+ClockPedro@users.noreply.github.com>
2023-10-19 10:59:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('essa página contém virus. Para continuar forneça seus dados bancários')
+
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();}
+document.querySelector('.tecla_pom').onclck = tocaSomPom;
+document.querySelector('#som_tecla_pom').play
+document.querySelector('.tecla_clap')
+document.querySelector('.tecla_tim')
+document.querySelector('.tecla_puff')
+document.querySelector('.tecla_splash')
+document.querySelector('.tecla_toim')
+document.querySelector('.tecla_psh')
+document.querySelector('.tecla_tic')
+document.querySelector('.tecla_tom')
